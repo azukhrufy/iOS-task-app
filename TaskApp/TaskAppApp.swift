1fd9b3f0c22f709164feb1403a72198b775152be
azukhrufy <anandazukhrufawalwi@gmail.com>
2022-04-10 02:32:55 +0700

    Initial Commit

---
+++ TaskApp/TaskAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  TaskAppApp.swift
+//  TaskApp
+//
+//  Created by mac on 10/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
